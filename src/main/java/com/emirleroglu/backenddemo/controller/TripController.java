8ac9eb7aa9cc33c5135b502814bc79d9053f6f0a
halilemirleroglu <emirlerogluhalil@gmail.com>
2021-11-09 23:22:07 +0300

    Cross Origin

--- src/main/java/com/emirleroglu/backenddemo/controller/TripController.java
+++ src/main/java/com/emirleroglu/backenddemo/controller/TripController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @Api(value = "Trip Api Documentation")
+@CrossOrigin
 public class TripController {
 
     @Autowired
--- src/main/java/com/emirleroglu/backenddemo/controller/UserController.java
+++ src/main/java/com/emirleroglu/backenddemo/controller/UserController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-
+@CrossOrigin
 public class UserController {
     @Autowired
     UserService myService;
