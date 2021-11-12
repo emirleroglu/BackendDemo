6a2978004eee5fedff687d209cd4312ab7b4a2f4
halilemirleroglu <emirlerogluhalil@gmail.com>
2021-11-12 20:55:36 +0300

    Swagger version 3

--- src/main/java/com/emirleroglu/backenddemo/BackendDemoApplication.java
+++ src/main/java/com/emirleroglu/backenddemo/BackendDemoApplication.java
@@ -13,14 +13,7 @@ public class BackendDemoApplication {
         SpringApplication.run(BackendDemoApplication.class, args);
     }
 
-    // @Bean
-    //public WebMvcConfigurer corsConfigurer() {
-    //  return new WebMvcConfigurer() {
-    //    @Override
-    //  public void addCorsMappings(CorsRegistry registry) {
-    //     registry.addMapping("/**").allowedOrigins("*");
-    // }
-    // };
+
 }
 
 
